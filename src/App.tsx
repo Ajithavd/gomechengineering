a6fb88e2555fe8a2cb6559ec3eafdb7a55d04bb6
ajithfsd <ajithfsd@gmail.com>
2026-09-08 16:51:13 +0530

    Fix React Router base path for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gomechengineering">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/gallery" element={<Gallery />} />
