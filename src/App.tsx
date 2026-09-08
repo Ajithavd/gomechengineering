f33878b0711fb5140de28073723bfcce89ab5ec9
ajithfsd <ajithfsd@gmail.com>
2026-09-08 18:26:13 +0530

    Fix routing for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/gomechengineering">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/gallery" element={<Gallery />} />
